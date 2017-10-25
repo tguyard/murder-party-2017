2dfc4eb9b8794efa054b614481c760e54dda2d55
Thomas Guyard <t.guyard@gigatribe.com>
2017-10-25 12:29:09 +0200

    improve tele

--- src/app/tele.component.ts
+++ src/app/tele.component.ts
@@ -12,16 +12,19 @@ export class TeleComponent {
   public mots: IncantationMot[] = [];
   public testResult = '';
   public position: [number, number] = [0, 0];
+  public currentLevelIndex = 0;
+  public orientation = 0;
 
-  private level0 = [
+  private origin: [number, number] = [19, 13];
+  private levels = [[
     '0211111201111111111201111111111111111112',
     '73655                                  3',
     '7 111  37                              3',
-    '655555 471         47                  3',
-    '011111 2027       527                  3',
+    '655555 471        547                  3',
+    '011111 2027      3027                  3',
     '7      3737      3737                  3',
     '7      3737      3737                  3',
-    '655555 47        3737                  3',
+    '655555 47          37                  3',
     '011111 27          37                  3',
     '7      365        54655 555 5555 55    3',
     '7      3027      301120 120 1201 127   3',
@@ -34,9 +37,7 @@ export class TeleComponent {
     '65555554                  67 37        3',
     '                           6547        3',
     '                              6555555554',
-  ];
-
-  private level1 = [
+  ], [
     'x  0112011111111111111120112',
     '   7      55555555         3',
     '   655437301111112737  365 4',
@@ -52,33 +53,56 @@ export class TeleComponent {
     '                    7  37  3',
     '                    7  37  3',
     '                    65546554',
-  ];
-
-  private levelB0 = [
+  ], [
     '01110112',
     '7 54   3',
     '7      3',
     '7      3',
     '65555554',
-  ];
-  private levelB1 = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
+  ], [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
        '  012',
        '  7 3',
        '  7 3',
        '  653',
        '  654',
+  ]];
+
+  public parcour = '';
+  public currentParcour: string[] = [];
+  private emptyLevel = [
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
+    '                                        ',
   ];
 
   constructor(
@@ -109,9 +133,11 @@ export class TeleComponent {
       this.testResult = 'wrong';
       return;
     }
-    let orientation = 160;
+    let orientation = 160 + this.orientation;
     let vitesse = 0;
-    let position: [number,number] = [19, 13];
+    let position: [number, number] = this.origin;
+    this.currentLevelIndex = 0;
+    this.currentParcour = this.emptyLevel;
     for (let i = 1; i < this.mots.length - 1; ++i) {
       switch (this.mots[i]) {
         case 'gadoola':
@@ -135,7 +161,7 @@ export class TeleComponent {
         }
         try {
           position = this.move(position, orientation, vitesse);
-        } catch(e) {
+        } catch (e) {
           this.testResult = 'wall';
           return;
         }
@@ -148,11 +174,12 @@ export class TeleComponent {
     }
 
     this.position = position;
+    this.parcour = this.currentParcour.join('\n');
     this.testResult = 'position';
   }
 
   private getValueAt(from: [number, number]) {
-    return this.level0[from[1]][from[0]];
+    return this.levels[this.currentLevelIndex][from[1]][from[0]];
   }
 
   private getAllowedOrientations(value: string) {
@@ -192,29 +219,46 @@ export class TeleComponent {
       case 3:
       return [from[0] + 1, from[1]];
       default:
-      throw new Error("What ??");
+      throw new Error('What ??');
     }
   }
 
   private move(from: [number, number], orientation: number, distance) {
 
     if (distance < 0) {
-      throw new Error("distance should be > 0");
+      throw new Error('distance should be > 0');
     }
     if (from[0] < 0 || from[1] < 0) {
-      throw new Error("from should be > 0");
+      throw new Error('from should be > 0');
     }
+
+    const tmp = this.currentParcour[from[1]];
+    this.currentParcour[from[1]] = tmp.substr(0, from[0]) + 'x' + tmp.substr(from[0] + 1);
 
     if (distance === 0) {
       return from;
     }
     const ori = orientation % 4;
-    console.log("move", from, distance, ori, this.getValueAt(from));
+    console.log('move', from, distance, ori, this.getValueAt(from));
     const allowedOrientations = this.getAllowedOrientations(this.getValueAt(from));
     if (allowedOrientations.find(v => v == ori) === undefined) {
-      throw new Error("C'est un mur");
+      throw new Error('C\'est un mur');
     }
-    return this.move(this.moveOne(from, ori), orientation, distance - 1);
+    const p = this.moveOne(from, ori);
+    if (this.currentLevelIndex === 0 || this.currentLevelIndex === 1) {
+      if (p[1] === 5 && (p[0] === 8 || p[0] === 9 || p[0] === 18 || p[0] === 19)) {
+        this.currentLevelIndex = this.currentLevelIndex === 0 ? 1 : 0;
+      }
+    } else if (this.currentLevelIndex === 0 || this.currentLevelIndex === 2) {
+      if (p[0] === 3 && (p[1] === 0 || p[1] === 1)) {
+        this.currentLevelIndex = this.currentLevelIndex === 0 ? 2 : 0;
+      }
+    } else if (this.currentLevelIndex === 0 || this.currentLevelIndex === 3) {
+      if (p[0] === 1 && p[1] === 17) {
+        this.currentLevelIndex = this.currentLevelIndex === 0 ? 3 : 0;
+      }
+    }
+    return this.move(p, orientation, distance - 1);
   }
 
   public ok() {
@@ -222,61 +266,3 @@ export class TeleComponent {
     this.position = [0, 0];
   }
 }
-
-/*
-
-'0211111201111111111201111111111111111112',
-'73655                                  3',
-'7 111  37                              3',
-'655555 47          47                  3',
-'011111 2027       527                  3',
-'7      3737      3737                  3',
-'7      3737      3737                  3',
-'655555 47        3737                  3',
-'011111 27          37                  3',
-'7      365        54655 555 5555 55    3',
-'7      3027      301120 120 1201 127   3',
-'655555 47365    54   37           37   3',
-'011111 27 127  301   37  37  37   37   3',
-'0                        37  37   37   3',
-'0      365546  4655554655465 465 5465  4',
-'0 55   3                 011 20  1111  2',
-'0 126  3                 67            3',
-'65555554                  67 37        3',
-'                           6547        3',
-'                              6555555554',
-
-'x  0112011111111111111120112',
-'   7      55555555         3',
-'   655437301111112737  365 4
-'   0112373        737  301 2',
-'   7   373        737      3',
-'   7  3373        736 5465 4',
-'   6554373        73030120 2',
-'   011136455555555647   27 3',
-'   7   3            73  37 3',
-'   65554            7 120  3',
-'                    7  37  3',
-'                    7      3',
-'                    7  37  3',
-'                    7  37  3',
-'                    65546554',
-
-
-   '01110112',
-   '7 54   3',
-   '7      3',
-   '7      3',
-   '65555554',
-
-
-
-
-  '  012',
-  '  7 3',
-  '  7 3',
-  '  653',
-  '  654',
-
-
-*/
