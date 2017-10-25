3bc0723ad6cc432e83a62279458a27d19765e0a1
Thomas Guyard <thomas.guyard@gmail.com>
2017-10-26 01:18:30 +0200

    update

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -6,6 +6,7 @@ import { Fragment, Words, FragmentService } from './fragment.service';
 export class SearchService {
 
   private fragments: Fragment[];
+  public tested: string[];
 
   public constructor(private users: UserService, fragmentService: FragmentService) {
     this.fragments = fragmentService.fragments;
@@ -20,8 +21,21 @@ export class SearchService {
       if (f.user !== this.users.getCurrentUser()) {
         return false;
       }
+      let found = false;
       for (let i = 0; i < idea.length; ++i) {
         if (f.words[0].find(w => w === idea[i]) !== undefined) {
+          found = true;
+          if (f.word2 == null) {
+            return true;
+          }
+        }
+      }
+      if (f.word2 == null || !found) {
+        return false;
+      }
+
+      for (let j = 0; j < idea.length; ++j) {
+        if (f.word2.find(w => w === idea[j]) !== undefined) {
           return true;
         }
       }
