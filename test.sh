a3c5d62edd73c0a20388fd787b4f96f35a1be996
Thomas Guyard <thomas.guyard@gmail.com>
2017-10-26 22:08:38 +0200

    up

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in https://tguyard.github.io/assets/fabien.html \
+    https://tguyard.github.io/assets/anne.html \
+    https://tguyard.github.io/assets/nico.html \
+    https://tguyard.github.io/assets/annelise.html \
+    https://tguyard.github.io/assets/christophe.html \
+    https://tguyard.github.io/assets/sabine.html \
+    https://tguyard.github.io/assets/adele.html ; do  
+
+# firefox -no-remote -CreateProfile $(basename $i)
+firefox -no-remote -P $(basename $i) $i &
+done
